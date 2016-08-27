75b5899f84fd66e6f2117f4eefed19a5ba6f2427
Douglas Casserleigh <casserleigh@gmail.com>
2016-08-26 20:15:05 -0600

    adding dev server

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<h1 style="font-family: Impact"> Hello There.</h1>
-<img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2016/03/1458289957powerful-images3.jpg"/>
-
-</body>
-
-</html>
